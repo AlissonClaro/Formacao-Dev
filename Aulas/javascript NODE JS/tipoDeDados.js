3a4c2075bb924b09bad67c13abb202cc8e4ee8c7
AlissonClaro <allnclaro@outlook.com>
2024-05-21 01:55:17 -0300

    iniciação javascript

---
+++ Aulas/javascript NODE JS/tipoDeDados.js
@@ -0,0 +1,20 @@
+/*
+1 - Strings -> textos
+
+"texto com aspas 'duplas' "
+'textos com aspas simples'
+`textos com crase`
+
+
+
+
+*/
+
+
+const texto1 = "texto com aspas 'duplas' "
+const texto2 = 'texto com aspas simples'
+const texto3 = `textos com crase`
+
+console.log(texto1)
+console.log(texto2)
+console.log(texto3)
---
+++ Aulas/javascript NODE JS/variaveis.js
@@ -0,0 +1,22 @@
+/*
+
+Variaveis
+
+ - let -> pode sofrer variação na variavel sobrer mudança de valor.
+    EX:
+        let variavelUm = 40
+
+        abacate = 30
+        --------------------------------
+
+ - const -> Valor não pode ser declarado novamente
+    EX:
+        const variavelUm = 40
+        ---------------------------------
+
+ - var -> descontinuado / nao usar!
+
+*/
+
+const variavel = 40
+    console.log(variavel)
