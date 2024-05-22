87c1efd83f06d7915e50251264bc0d4eecbe6048
AlissonClaro <allnclaro@outlook.com>
2024-05-22 13:04:03 -0300

    atualização de aulas 2

---
+++ Aulas/javascript NODE JS/12 - functions.js
@@ -0,0 +1,20 @@
+/* 
+
+    Functions / Funções
+
+        Trecho de codigo que é executado
+        SOMENTE quando solicitado ( Parente gente boa)
+
+*/
+
+
+const variavel = 'fulano'
+
+console.log(variavel)
+
+function nomeNaTela() {
+
+        console.log("Alisson")
+}
+
+nomeNaTela()
