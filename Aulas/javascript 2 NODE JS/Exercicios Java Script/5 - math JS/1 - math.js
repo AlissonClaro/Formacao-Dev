f9171a5d39d76cc2238c477d8a44581edb9b4d7e
AlissonClaro <allnclaro@outlook.com>
2024-06-04 12:12:14 -0300

    aulas

---
+++ Aulas/javascript 2 NODE JS/Exercicios Java Script/5 - math JS/1 - math.js
@@ -0,0 +1,34 @@
+/*
+
+Math - Matematica
+
+- pow(2,2) / potencia / 2² = 4
+- sqrt(25,2) / raiz / √25
+- PI / 3,14
+- ceil / teto -> arendonda para cima
+- floor / chão -> arredonda para baixo
+
+const result = Math.pow(5,3)
+console.log(result)
+
+const result = Math.sqrt(25)
+console.log(result)
+
+const result = Math.PI
+console.log(result)
+
+const result = Math.ceil(3.14)
+console.log(result)
+
+const result = Math.floor(3.14)
+console.log(result)
+
+
+
+
+- random() / numero aleatorio entre 0 e 1
+
+*/
+
+const result = Math.floor(3.14)
+console.log(result)
