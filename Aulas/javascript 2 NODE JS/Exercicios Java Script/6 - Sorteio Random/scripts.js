d105fdfe51511b5ef3d51b51e0697b4b76e18ab3
AlissonClaro <allnclaro@outlook.com>
2024-06-07 17:47:15 -0300

    colocando if numMax<numMin

--- Aulas/javascript 2 NODE JS/Exercicios Java Script/6 - Sorteio Random/scripts.js
+++ Aulas/javascript 2 NODE JS/Exercicios Java Script/6 - Sorteio Random/scripts.js
@@ -4,6 +4,11 @@ function generateNumber() {
     const numMin = Math.ceil(document.querySelector('.input-min').value)
     const numMax = Math.floor(document.querySelector('.input-max').value)
 
+    if (numMax <= numMin) {
+        alert('O numero maximo é menor que o numero minimo')        
+    } 
+    else {
     const result = Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
     document.getElementById('result').textContent = 'O número sorteado é: ' + result +' 👈🏻';
+    }
 }
