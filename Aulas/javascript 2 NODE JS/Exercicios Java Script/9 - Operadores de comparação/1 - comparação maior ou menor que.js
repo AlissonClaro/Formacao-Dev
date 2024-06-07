40bac0125ea95ad2cc64ae46a276092df62b20ef
AlissonClaro <allnclaro@outlook.com>
2024-06-07 17:39:19 -0300

    atualização de aulas

---
+++ Aulas/javascript 2 NODE JS/Exercicios Java Script/9 - Operadores de comparação/1 - comparação maior ou menor que.js
@@ -0,0 +1,34 @@
+/* 
+
+>  -> Maior
+<  -> Menor
+
+>= Maior ou Igual
+<= Menor ou Igual
+
+Exemplo...................
+
+const numberOne = 20
+const numberTwo = 30
+
+console.log(numberOne < numberTwo)
+true
+------------------------------
+
+const numberOne = 20
+const numberTwo = 30
+
+console.log(numberOne > numberTwo)
+false
+--------------------------------
+
+
+
+
+*/
+
+const numberOne = 20
+const numberTwo = 30
+
+console.log(numberOne > numberTwo)
+false
