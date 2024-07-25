92f250cdaa2eeb3cddf7734a5be5704c2f2ff833
Alisson Claro <alissonclaroo@outlook.com>
2024-07-25 14:18:57 -0300

    build Node

--- Conceitos Node Aulas/1 - Node/server.js
+++ Conceitos Node Aulas/1 - Node/server.js
@@ -1,6 +1,42 @@
+/*HTTP METODOS
+    *Verbos
 
-import express from 'express'
+
+Query Params
+Get -> Listar
+
+
+Post -> Criar
+Put -> Editar Varios
+Patch -> Editar um
+Delete -> Deletar
+
+
+
+*/
+
+
+import express, { request, response } from 'express'
 
 const app = express()
 
+app.use(express.json())
+
+app.get('/usuarios/:Merlyn', (req, res) => {
+    
+    console.log (req)
+
+    res.send ('Ola rota acessada com sucesso!')
+})
+
+app.post('/usuarios', (req, res) => {
+
+    console.log (req)
+
+    res.send ('Ola esta é a rota post!')
+})
+
 app.listen(3000)
+//http://localhost:3000
+//req = request - requisição
+//res = response - resposta
