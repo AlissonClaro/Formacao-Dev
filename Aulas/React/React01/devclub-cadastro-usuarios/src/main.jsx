c4dfa7b61f6e4dad885212b9d5ba358f470564f8
Alisson Claro <alissonclaroo@outlook.com>
2024-09-04 21:21:27 -0300

    att React

--- Aulas/React/React01/devclub-cadastro-usuarios/src/main.jsx
+++ Aulas/React/React01/devclub-cadastro-usuarios/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' //Default
+
+//import {App, App2 as QualquerCoisa} from './App.jsx' //export sem Default - e renomear a variavel
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
